1d2a182d3eddea915269769dc2baa4263ca13f32
MD ANWAR JAHID <ajr.jahid@gmail.com>
2018-01-24 16:41:12 +0600

    Twitter & Linkdin

--- src/SoshareController.php
+++ src/SoshareController.php
@@ -7,112 +7,19 @@ use App\Http\Controllers\Controller;
 
 class SoshareController extends Controller
 {
-    public function soshare_script($selector)
-    {
-    	//$my_file = 'soshare.js';
-		//$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-		$data = $this->jscode($selector,'http://bdaliens.com');
-		//fwrite($handle, $data);
-		return $data;
-    }
 
-    private function jscode($selector,$url,$customclass=null)
-    {
-    	return <<<EOT
-
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
-
-  <!-- Your share button code -->
-  <div class="fb-share-button" 
-    data-href="https://bdaliens.com" 
-    data-layout="button_count">
-  </div>
-EOT;
-    }
-
-
-
-    public function fb_markup($class = null)
-    {
-    	return <<<EOT
-    	<div class="shareBtn btn btn-success clearfix" class="">Share</div>
-    	<p class="$class">
-  <hr />
-</p>
-EOT;
-    }
-
-
-    public function fb_script2()
-    {
-    	return <<<EOT
-    	<script>
-document.getElementsByClassName('shareBtn').onclick = function() {
-  FB.ui({
-    method: 'share',
-    display: 'popup',
-    href: 'https://developers.facebook.com/docs/',
-  }, function(response){});
-}
-</script>
-EOT;
-    }
-
-
-    public function fb_script3()
-    {
-    	return <<<EOT
-<script>
-
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : 1652012351544822,
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.11'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-    	$('document').ready(function(){
-    		$(document).on('click','.shareBtn',function(){
-    			FB.ui({
-			    method: 'share',
-			    display: 'popup',
-			    href: 'https://developers.facebook.com/docs/',
-			  }, function(response){});
-    		})
-    	})
-</script>
-EOT;
-    }
-
-public function fblinkshare($selector,$urlselector)
+public function fblinkshare($selector)
     {
       $abc = config('soshare.facebook.url');
       return <<<EOT
    <script>
 
    $(document).ready(function(){
-    var prothomongsho = "$abc"
-    var ditioongsho   = $("$selector").attr('href')
-    var shareurl = prothomongsho + ditioongsho
-
     $(document).on('click',"$selector",function(){
-      console.log(shareurl)
+    
+      var prothomongsho = "$abc"
+      var ditioongsho   = $(this).attr('href')
+      var shareurl = prothomongsho + ditioongsho
       window.open(shareurl,'_blank','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=300,height=300');return false;
     })
    })
